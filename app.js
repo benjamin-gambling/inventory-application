2e858b7e70280d045510b1fb30156c0f99a4eb3c
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-08-30 15:35:15 -0700

    set configs

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ hbs.registerHelper("if_eq", function (a, b, opts) {
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = process.env.MONGO_URL;
+var mongoDB = process.env.MONGODB_URI || process.env.MONGO_URL;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
